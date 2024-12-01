1f07fac555d041f2a0aa4447cec475143e7e91ea
abdullahcseexe <abduladilcse.exe@gmail.com>
2024-12-02 00:01:40 +0530

    second commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import Colors from '@/src/constants/Colors';
+import { Image, StyleSheet, Text, View} from 'react-native';
+import products from '@/assets/data/products';
+import ProductListItem from '../../components/ProductListItem'
+
+
+
+export default function TabOneScreen() {
+  return (
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
+    </View>
+  );
+}
+
+;
